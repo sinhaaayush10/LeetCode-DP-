f180e5a7b72f8dc12ae4b682762f432c00458c50
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2023-11-01 11:43:53 +0530

    Find The Original Array of Prefix Xor.py

---
+++ Find The Original Array of Prefix Xor.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        lst = []                            # result
+        dic = {}                            # put node value in dictionary
+        maxi = 0                            # the frequence of the most frequently occurred element
+        
+        def find(root):
+            if not root:                    # leaf node
+                return
+            if root.val in dic:             # if node value is in the dictionry, value + 1
+                dic[root.val] += 1
+            else:
+                dic[root.val] = 1           # if node value is not in the dictionary, value = 1
+            find(root.left)
+            find(root.right)
+            return 
+        
+        find(root)
+        dicval_list = list(dic.values())    # list all the values in dictionary
+        maxi = max(dicval_list)             # find maximum value in dictionary
+        
+        for key, value in dic.items():
+            if value == maxi:               # if value = maximum, append this key to list
+                lst.append(key)
+        return lst
+
+        # Solution 2
+    def helper(self, root, cache):
+        if root == None:
+            return
+        cache[root.val] += 1
+        self.helper(root.left, cache)
+        self.helper(root.right, cache)
+        return
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+        cache = defaultdict(int)
+        self.helper(root, cache)
+        max_freq = max(cache.values())
+        result = [k for k,v in cache.items() if v == max_freq]
+        return result
