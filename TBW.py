09db2ac2b456f1f862d26993f22839a4bfbc5efc
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2023-11-06 11:34:13 +0530

    Update TBW.py

--- TBW.py
+++ TBW.py
@@ -1,1 +1,22 @@
-Will Code Tmrw
+class SeatManager {
+public:
+    priority_queue<int, vector<int>, greater<int>>pq;
+    int c = 0;
+    SeatManager(int n) {    
+        
+    }
+    
+    int reserve() {
+        if (pq.size() && pq.top() <= c) {
+            int t = pq.top();
+            pq.pop();
+            return t;
+        }
+        c++;
+        return c;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);
+    }
+};
