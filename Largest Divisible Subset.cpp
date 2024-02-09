d6dfe44360b588f746bb2056134a148df823638d
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-02-09 21:25:46 +0530

    Update Largest Divisible Subset.cpp

--- Largest Divisible Subset.cpp
+++ Largest Divisible Subset.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n=nums.size(), maxi=1, num=-1;
         vector<int>v;
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); 
         vector<int>dp(n, 1);
         for(int i=1; i<n; i++){
             for(int j=0; j<i; j++){
