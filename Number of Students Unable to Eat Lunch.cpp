d28e55a275ec0a487b5568a4191b6c8068024e89
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-04-08 13:56:32 +0530

    Number of Students Unable to Eat Lunch.cpp

--- Number of Students Unable to Eat Lunch.cpp
+++ Number of Students Unable to Eat Lunch.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+    int countStudents(vector<int>& students, vector<int>& sandwiches) { 
         queue<int> q;
 
         for(int x : students) {
