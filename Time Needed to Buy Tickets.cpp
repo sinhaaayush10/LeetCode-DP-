461c306f071853e21f2d734aabbd2fa9fb9d0aa6
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-04-09 12:32:17 +0530

    Time Needed to Buy Tickets.cpp

--- Time Needed to Buy Tickets.cpp
+++ Time Needed to Buy Tickets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int timeRequiredToBuy(vector<int>& tickets, int k) {
+    int timeRequiredToBuy(vector<int>& tickets, int k) { 
         int total = 0;
 
         for (int i = 0; i < tickets.size(); ++i) {
