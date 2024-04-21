1e15c9d429a0507c3d12158ef0152066850c1c7b
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-04-21 15:16:43 +0530

    Find if Path Exists in Graph.cpp

--- Find if Path Exists in Graph.cpp
+++ Find if Path Exists in Graph.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) { 
         unordered_map<int, vector<int>> graph;
-        for (const auto& edge : edges) {
+        for (const auto& edge : edges) { 
             int u = edge[0];
             int v = edge[1];
             graph[u].push_back(v);
