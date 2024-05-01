74422f56ffbb5b59a4a5ff631537124bdddd8f72
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-05-01 23:36:32 +0530

    Reverse Prefix of Word.cpp

--- Reverse Prefix of Word.cpp
+++ Reverse Prefix of Word.cpp
@@ -3,7 +3,7 @@ public:
     string reversePrefix(string& word, char ch) {
         int r=word.find(ch);
         if (ch==-1) return word;
-        int r0=(r-1)/2;
+        int r0=(r-1)/2; 
         for(int i=0; i<=r0; i++)
             swap(word[i], word[r-i]);
         return word;
