4cec175fd5b8ff1d3e48d17b3891f98416f459e5
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-07-10 23:56:46 +0530

    Crawler Log Folder.cpp

---
+++ Crawler Log Folder.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static int minOperations(vector<string>& logs) {
+        int level=0;
+        for(auto& dir: logs){
+            if (dir=="../") 
+                level-=(level>0);
+            else if (dir!="./")
+                level++;
+        }
+        return level;
+    }
+};
+auto init =[]{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+};
